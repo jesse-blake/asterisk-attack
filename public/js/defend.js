1f694ea4a11e806262e18c1c05588f610651f494
Jesse Blake <jesse@littlesystem.com>
2016-11-24 01:21:28 -0600

    Fix: heatbeams/defender go under and thru menubar

--- public/js/defend.js
+++ public/js/defend.js
@@ -4,8 +4,8 @@ var asteriskAttack = (function(aa) {
     var pos = aa.dom.defender.offset();
 
     var heatbeam = $('<div id="heatbeam' + (++aa.game.heatbeamCount) + '" '
-      + 'style="color:deeppink; position:absolute; '
-      + 'top:' + (pos.top - 110) + 'px; '
+      + 'style="color:deeppink; position:absolute; z-index: 1; '
+      + 'top:' + (pos.top - 45) + 'px; '
       // Line up heatbeam with the eyes of the defender.
       + 'left:' + (pos.left + (aa.dom.defender.width() / 2)) + 'px;'
       + '">|<br>|<br>|<br></div>');
@@ -13,7 +13,7 @@ var asteriskAttack = (function(aa) {
     aa.dom.zones.prepend(heatbeam);
     aa.game.heatbeams[aa.game.heatbeamCount] = heatbeam;
 
-    heatbeam.animate({ top: '-50px' }, 500, 'linear', function() {
+    heatbeam.animate({ top: '-150px' }, 500, 'linear', function() {
       delete aa.game.heatbeams[heatbeam.attr('id').split('').pop()];
       heatbeam.remove();
     });
